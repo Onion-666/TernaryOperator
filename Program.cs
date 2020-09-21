954a8ac98ead95b4cba49664ec92a81a550a52de
Халяпин Вадим <vadim.halyapin@yandex.ru>
2020-09-21 20:46:42 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TernaryOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string temp = Console.ReadLine();
+            int x = Int32.Parse(temp);
+            int num = (x < 0) ? 1 : 2;
+            Console.WriteLine(num);
+        }
+    }
+}
